7436c0c232c79cb99f5921044d35a3f7f8347164
Wojtek <42886283+WojtekFront@users.noreply.github.com>
2021-04-05 19:16:04 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+
+
+    <div class="wrapper">
+        <div id="banner">
+        <a href="/" title="Strona Główna">
+        <img src="img/banner.jpg" alt="tło">
+        </a>
+        </div>
+
+        <div id="nav">
+<ul>
+    <li><a href="">Biuro Nieruchmości</a></li>
+    <li><a href="">Oferta</a></li>
+    <li><a href="">Kontakt</a></li>
+</ul>
+
+
+
+        </div> <!-- nav -->
+        <div class="content">
+            <div class="footer">
+                <div class="column">
+                    <strong>Telefon</strong>
+                    555-555-555
+                </div><!-- column -->
+                <div class="column">
+                    <strong>Miejsce</strong>
+                    Szczecin 77-777<br>
+                    Szczecińska 1/1
+                </div><!-- column -->
+                <div class="column">
+                    <strong>Godziny otwarcia</strong>
+                    <em>Poniedziałek-piatek</em><br>
+                    9:00 - 17:00<br><br>
+                    <em>Sobota</em><br>
+                    9:00 - 13:00<br><br>
+                    <em>Niedziela:</em><br>
+                    Zamknięte<br><br>
+                </div><!-- column -->
+            </div><!-- footer -->
+            <small>&copy<?php echo date("Y"); ?> <b>WojtekFront</b></small>
+        </div><!-- content -->
+    </div><!-- wrapper -->
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
