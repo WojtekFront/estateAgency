e0f209642b6915371309a9c18fb1c1edbc403f61
Wojtek <42886283+WojtekFront@users.noreply.github.com>
2021-04-10 11:05:54 +0200

    more houses

--- setData/houses.php
+++ setData/houses.php
@@ -1,21 +1,84 @@
 <?php
 $offers = array(
  "offer1" =>  array(
-        "city" => "Trzebież",
+        "city" => "Trzebież 1",
         "price" => "100",
         "describe" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ",
         "picture1" => "lakHouse1",
         "picture2" => "lakHouse2"
     ),
     "offer2"=>  array(
-        "city" => "Trzęsacz",
+        "city" => "Trzęsacz 2",
         "price" => "300",
         "describe" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ",
         "picture1" => "larHouse1",
         "picture2" => "larHouse2"
     ),
     "offer3" => array(
-        "city" => "Goleniów",
+        "city" => "Goleniów 3",
+        "price" => "70",
+        "describe" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ",
+        "picture1" => "scanHouse1",
+        "picture2" => "scanHouse2"
+    ),
+    "offer4" =>  array(
+        "city" => "Trzebież 4",
+        "price" => "100",
+        "describe" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ",
+        "picture1" => "lakHouse1",
+        "picture2" => "lakHouse2"
+    ),
+    "offer5"=>  array(
+        "city" => "Trzęsacz 5",
+        "price" => "300",
+        "describe" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ",
+        "picture1" => "larHouse1",
+        "picture2" => "larHouse2"
+    ),
+    "offer6" => array(
+        "city" => "Goleniów 6",
+        "price" => "70",
+        "describe" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ",
+        "picture1" => "scanHouse1",
+        "picture2" => "scanHouse2"
+    ),
+    "offer7" =>  array(
+        "city" => "Trzebież 7",
+        "price" => "100",
+        "describe" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ",
+        "picture1" => "lakHouse1",
+        "picture2" => "lakHouse2"
+    ),
+    "offer8"=>  array(
+        "city" => "Trzęsacz 8",
+        "price" => "300",
+        "describe" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ",
+        "picture1" => "larHouse1",
+        "picture2" => "larHouse2"
+    ),
+    "offer9" => array(
+        "city" => "Goleniów 9",
+        "price" => "70",
+        "describe" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ",
+        "picture1" => "scanHouse1",
+        "picture2" => "scanHouse2"
+    ),
+    "offer10" =>  array(
+        "city" => "Trzebież 10",
+        "price" => "100",
+        "describe" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ",
+        "picture1" => "lakHouse1",
+        "picture2" => "lakHouse2"
+    ),
+    "offer11"=>  array(
+        "city" => "Trzęsacz 11",
+        "price" => "300",
+        "describe" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ",
+        "picture1" => "larHouse1",
+        "picture2" => "larHouse2"
+    ),
+    "offer12" => array(
+        "city" => "Goleniów 12",
         "price" => "70",
         "describe" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ",
         "picture1" => "scanHouse1",
