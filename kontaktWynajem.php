97ca30fc0f01a3e95992956cada6ad725e007309
Wojtek <42886283+WojtekFront@users.noreply.github.com>
2021-04-12 21:26:28 +0200

    kontakt
    few mistake

--- kontaktWynajem.php
+++ kontaktWynajem.php
@@ -3,23 +3,25 @@ define("title", "kontakt|| biuro nieruchomości");
 include("includes/header.php");
 ?>
 <div class="contact">
-    <h1>Masz jakieś pytanie</h1>
+    <h1>Masz jakieś pytanie</h1><br>
     <form method="post" action="" id="contactForm">
 
-        <label id="yourEmail">Twoje imię:</label>
-        <input type="text" name="yourEmail"><br>
+        <label for="yourName">Twoje imię:  </label>
+        <input type="text" name="yourName" placeholder="  imię"><br>
 
-        <label for="yourEmail">twój e-mail:</label>
-        <input type="email" name="yourEmail"><br>
+        <label for="yourEmail">Twój e-mail:  </label>
+        <input type="email" name="yourEmail" placeholder="  e-mail"><br>
 
-        <label for="yourMessage">Napisz wiadomość:</label>
-        <textarea name="yourMessage" cols="30" rows="10"></textarea><br>
+        <label for="yourMessage">Napisz wiadomość:  </label>
+        <textarea name="yourMessage" cols="30" rows="10" placeholder="  wiadomość"></textarea><br>
 
         <input type="checkbox" id="subscribe" name="subscribe" value="subscribe" >
-        <label for="subscribe">wysyłaj newsletter</label><br>
-        <input type="submit" class="submit" name="contact_submit" value="wyślij">
+        <label for="subscribe" class="subscribe">wysyłaj newsletter</label><br>
+        
+        <input type="submit" class="submit" name="contact_submit" value="WYŚLIJ">
  </form>
-</div>
+ <br>
+</div><!-- contact -->
 
 <?php
 include("includes/footer.php");
